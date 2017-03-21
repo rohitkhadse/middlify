b66f16681383819996bb28a7ac5b6cca9dc5b9fe
rohitkhadse <rohit.a.khadse@gmail.com>
2017-03-20 19:24:08 -0500

    Online Code Editor

---
+++ demo/kitchen-sink/docs/javascript.js
@@ -0,0 +1,21 @@
+/*
+Middlify is online code editor which lets 
+you align your code around some character. 
+
+This not only improves the visibility of the code 
+but it also helps if you want to vertically edit the code. 
+Try it by hitting Middlify button on left bottom. 
+*/
+var filterStatus = $("#filterStatus").val();
+var filterby = $("#filterby").val();
+var filterContactType = $("#filterContactType").val();
+var filterSource = $("#filterSource").val();
+var filterRating = $("#filterRating").val();
+var filterTag = $("#filterTag").val();
+var filterColorCode = $("#filterColorCode").val();
+var filterblanknames = $("#filterblanknames").val();
+var search = $("#search").val();
+var managers = $("#managers").val();
+var filtersmsopted = $("#filtersmsopted").val();
+var filtervoiceopted = $("#filtervoiceopted").val();
+var filterdirectmail = $("#filterdirectmail").val();
